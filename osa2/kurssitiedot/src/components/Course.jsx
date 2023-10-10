35fde4f8d50c284e0c8d9daf2671866dd06ed058
aarnif <aarni.fagerstrom@gmail.com>
2023-10-10 09:56:14 +0300

    Tehtava 2.2 ja 2.3

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -28,11 +28,23 @@ const Part = ({ name, exercises }) => {
   );
 };
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <>
+      <p>
+        <strong>total of {total} exercises</strong>
+      </p>
+    </>
+  );
+};
+
 const Course = ({ course }) => {
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
