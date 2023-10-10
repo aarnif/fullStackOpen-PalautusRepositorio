8682c5ed274c1388d1d6e4662b7c044c2afab6d1
aarnif <aarni.fagerstrom@gmail.com>
2023-10-10 10:35:12 +0300

    Luo uusi react projekti puhelinluettelo ja tee tehtava 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = () => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onChange={handleChange}>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
