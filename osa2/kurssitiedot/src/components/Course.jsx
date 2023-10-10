37c021a4759e93b4e4a3fcb16203877a3900b2ca
aarnif <aarni.fagerstrom@gmail.com>
2023-10-10 09:39:49 +0300

    Luo uusi react projekti kurssitiedot ja tee tehtava 2.1

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Header = ({ name }) => {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <>
+      <p>
+        {name} {exercises}
+      </p>
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
