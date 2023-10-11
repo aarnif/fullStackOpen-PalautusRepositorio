e43ddb4a79b29044f07ab2044631056274620eda
aarnif <aarni.fagerstrom@gmail.com>
2023-10-11 16:12:11 +0300

    Tehtava 2.16

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -9,12 +8,14 @@ import {
   deletePerson,
   updatePhoneNumber,
 } from "./phoneService.js";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
   const [filterCondition, setFilterCondition] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     getAllPersons().then((data) => setPersons(data));
@@ -41,9 +42,17 @@ const App = () => {
         const newPersons = persons.filter(
           (person) => person.id !== deletePersonId
         );
+        displayErrorMessage(`Deleted ${person.name}`);
         setPersons(newPersons);
       });
     }
+  };
+
+  const displayErrorMessage = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 2000);
   };
 
   const handleSubmit = () => {
@@ -67,11 +76,13 @@ const App = () => {
               ? { ...person, number: newPhoneNumber }
               : person
           );
+          displayErrorMessage(`Updated phone number for ${newPerson.name}`);
           setPersons(updatedPersons);
         });
       }
     } else {
       addNewPerson(newPerson).then((res) => {
+        displayErrorMessage(`Added ${newPerson.name}`);
         setPersons(persons.concat(res));
       });
     }
@@ -86,6 +97,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm
