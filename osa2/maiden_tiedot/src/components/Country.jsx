46b86ff7613360d8ddbdcea8b9e09d301ac79b07
aarnif <aarni.fagerstrom@gmail.com>
2023-10-12 17:03:51 +0300

    Tehtava 2.20

--- osa2/maiden_tiedot/src/components/Country.jsx
+++ osa2/maiden_tiedot/src/components/Country.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [weatherReport, setWeatherReport] = useState(null);
+  const api_key = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${country.capital[0]}`
+      )
+      .then((res) => setWeatherReport(res.data));
+  }, []);
+
+  const convertToMetresPerSecond = (kilometersPerHour) => {
+    return ((kilometersPerHour * 1000) / 3600).toFixed(2);
+  };
+
   return (
     <>
       <h1>{country.name.common}</h1>
@@ -13,6 +29,16 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png}></img>
+      {weatherReport && (
+        <>
+          <h1>{`Weather in ${country.capital[0]}`}</h1>
+          <div>temperature {weatherReport.current.temp_c} Celcius</div>
+          <img src={weatherReport.current.condition.icon}></img>
+          <div>
+            wind {convertToMetresPerSecond(weatherReport.current.wind_kph)} m/s
+          </div>
+        </>
+      )}
     </>
   );
 };
